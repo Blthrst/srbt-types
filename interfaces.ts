69868f535ab3dad4612b27ec0bb0f2bb27ed202e
Blthrst <d.user@internet.ru>
2024-07-23 14:51:56 +0700

    init

--- interfaces.ts
+++ interfaces.ts
@@ -1,10 +1,6 @@
 import { PeersIndex, Message, TelegramClient } from "@mtcute/node";
 
-import {
-  UpdateTypes,
-  TypeUpdateOrTypeMessage,
-  UpdateType,
-} from "./types";
+import { UpdateTypes, TypeUpdateOrTypeMessage, UpdateType } from "./types";
 
 export interface ISrbtModule {
   name: string;
@@ -23,17 +19,5 @@ export interface ISrbtTrigger {
   getTrigger: () => string;
   getUpdateType: () => UpdateTypes;
 
-  // action(...args: any): Promise<void>;
-
-  // action(
-  //   trigger: TriggerTypes.RAW_UPDATE,
-  //   upd: tl.TypeUpdate | tl.TypeMessage,
-  //   peers: PeersIndex,
-  // ): Promise<void>;
-
   action(tg: TelegramClient, upd: UpdateType): Promise<void>;
-
-  // action(upd: Message, trigger: TriggerTypes.NEW_MESSAGE): Promise<void>;
-
-  // action(upd: Message, trigger: TriggerTypes.EDIT_MESSAGE): Promise<void>;
 }
